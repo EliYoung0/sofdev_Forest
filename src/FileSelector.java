38ece98e22ad3c19d372c62301f33daacaf36046
eliyoung <eliyoung@lclark.edu>
2019-02-14 14:37:33 -0800

    Added Comments

--- src/FileSelector.java
+++ src/FileSelector.java
@@ -19,15 +19,23 @@ public class FileSelector extends Container {
         add(open,BorderLayout.PAGE_END);
     }
 
+    /**
+     * Creates the Browser frame
+     * Contains a text field and browse button
+     * @return returns the Panel to be shown by FileSelector
+     */
     private JPanel createBrowser() {
         JPanel fb = new JPanel();
         fb.setLayout(new BoxLayout(fb,BoxLayout.X_AXIS));
         address = new JTextArea(1,20);
         JButton browse = new JButton("Browse");
+        //Opens a new window when browse button is clicked
         browse.addActionListener(e -> {
             JFileChooser chooser = new JFileChooser();
+            //Sets the allowed file extension types
             FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG & GIF Images", "jpg");
             chooser.setFileFilter(filter);
+            //Sets whether files or folders or both are allowed
             chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
             int returnVal = chooser.showOpenDialog(chooser);
             if(returnVal == JFileChooser.APPROVE_OPTION) {
@@ -39,6 +47,10 @@ public class FileSelector extends Container {
         return fb;
     }
 
+    /**
+     * Returns the file path text field object
+     * @return the address text field
+     */
     public JTextArea getAddress() {
         return address;
     }
