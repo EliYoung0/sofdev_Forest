7a067693548b604f260430f2eac716018e018b86
eliyoung <eliyoung@lclark.edu>
2019-02-26 14:51:53 -0800

    -Made UI not as bad

--- src/Thresholder.java
+++ src/Thresholder.java
@@ -1,6 +1,5 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -16,56 +15,85 @@ class Thresholder extends Container {
     private String filepath;
 
     Thresholder(String path, UI ui) {
-        filepath=path;
-        setLayout(new BorderLayout());
+        filepath = path;
+        setLayout(new GridBagLayout());
         JLabel imageLabel;
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx=0;
+        c.gridy=0;
+        c.gridheight=3;
         try {
             BufferedImage image = ImageIO.read(new File(path));
             int height = image.getHeight();
             int width = image.getWidth();
             Image i = image.getScaledInstance((500 * width) / height, 500, Image.SCALE_SMOOTH);
             imageLabel = new JLabel(new ImageIcon(i));
-            add(imageLabel,BorderLayout.LINE_START);
+            add(imageLabel, c);
         } catch (IOException ex) {
-            imageLabel=new JLabel();
+            imageLabel = new JLabel();
         }
+
         JPanel algPanel = new JPanel();
         algPanel.setLayout(new BoxLayout(algPanel, BoxLayout.Y_AXIS));
 
-        JTextField threshold = new JTextField();
+        JPanel threshPanel = new JPanel();
+        threshPanel.setLayout(new GridLayout());
+        threshPanel.setLayout(new GridBagLayout());
+
+        JTextField threshold = new JTextField(20);
+
         JButton update = new JButton("Update");
+
         JTextArea consoleOutput = new JTextArea("");
         consoleOutput.setEditable(false);
+        JScrollPane consoleScroll = new JScrollPane(consoleOutput);
 
-        update.addActionListener(new UpdateAction(path,imageLabel,threshold,this, consoleOutput));
-        JButton proceed = new JButton("Save & Exit");
+        update.addActionListener(new UpdateAction(path, imageLabel, threshold, this, consoleOutput));
+
 
         JLabel updateText = new JLabel("<html>Simple converter to black and white.<br>Enter the average RGB value which is the lower bound for white:   </html>");
 
+        c.gridheight=1;
+        c.gridx=1;
+        c.gridy=0;
+        add(updateText,c);
+        threshPanel.add(threshold);
+        threshPanel.add(update);
+        c.gridy=1;
+        add(threshPanel,c);
+        c.gridy=2;
+        add(consoleScroll,c);
 
-        algPanel.add(updateText);
-        algPanel.add(threshold);
-        algPanel.add(update);
-        algPanel.add(consoleOutput);
-        add(algPanel,BorderLayout.LINE_END);
-        proceed.addActionListener(e->{
-            saveBlack();
+        //Give proceed button functionality
+        JButton proceed = new JButton("Save & Exit");
+        proceed.addActionListener(e -> {
+            if(blackOutput!=null) {
+                saveBlack();
+            }
             System.exit(0);
         });
-        add(proceed,BorderLayout.PAGE_END);
+
+        //Add components to Container
+        c.gridx=0;
+        c.gridy=3;
+        c.gridwidth=2;
+        add(proceed, c);
 
     }
-    void setBlack(BufferedImage b){
-        blackOutput=b;
+
+    void setBlack(BufferedImage b) {
+        blackOutput = b;
     }
-    void saveBlack(){
+
+    private void saveBlack() {
         String newFilepath;
-        newFilepath = filepath.replaceAll("(.[a-zA-Z]{3,4}$)","_basic_"+currentThreshold+"_"+java.time.LocalDate.now()+"$1");
+        newFilepath = filepath.replaceAll("(.[a-zA-Z]{3,4}$)", "_basic_" + currentThreshold + "_" + java.time.LocalDate.now() + "$1");
         File outputFile = new File(newFilepath);
         try {
-            ImageIO.write(blackOutput,"jpg",outputFile);
+            ImageIO.write(blackOutput, "jpg", outputFile);
+        } catch (IOException ignored) {
         }
-        catch (IOException ignored){}
     }
 
     public BufferedImage getBlackOutput() {
@@ -83,39 +111,37 @@ class UpdateAction implements ActionListener {
     private String path;
     private Thresholder outer;
     private JTextArea console;
-    UpdateAction(String path, JLabel t, JTextField thresh,Thresholder outer, JTextArea output){
-        text=thresh;
-        this.t=t;
-        this.outer=outer;
-        this.path=path;
-        console=output;
+
+    UpdateAction(String path, JLabel t, JTextField thresh, Thresholder outer, JTextArea output) {
+        text = thresh;
+        this.t = t;
+        this.outer = outer;
+        this.path = path;
+        console = output;
     }
 
     public void actionPerformed(ActionEvent e) {
         try {
             int threshold = Integer.parseInt(text.getText());
-            if(threshold>=0&&threshold<=255){
-                try {
+            if (threshold >= 0 && threshold <= 255) {
+
                     BufferedImage bl = Black.makeBlack(path, threshold);
                     Image i = bl.getScaledInstance((500 * bl.getWidth()) / bl.getHeight(), 500, Image.SCALE_SMOOTH);
                     t.setIcon(new ImageIcon(i));
                     t.repaint();
-                    t.update(t.getGraphics());
                     outer.setBlack(bl);
                     outer.setCurrentThreshold(threshold);
                     //Remove in final product. Just to show functionality right now.
-                    console.append("\nGap Fraction is: "+Black.getGapFraction(bl));
-                }catch (IOException f){
-                    console.append("\nInvalid input image.");
-                }
+                    console.append("\nGap Fraction is: " + Black.getGapFraction(bl));
 
+            } else {
+                console.append("\nThreshold must be an integer between 0 and 255.");
             }
-            else{
-                console.append("\nThreshold must be between an integer between 0 and 255.");
-            }
-        }
-        catch (NumberFormatException f){
+        } catch (NumberFormatException f) {
             console.append("\nEnter a valid integer value.");
+        }
+        catch (IOException ex){
+            console.append("\nInvalid image file path");
         }
 
     }
