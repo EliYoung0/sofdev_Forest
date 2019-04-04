f38ee710248209e1f05cde3256ae35fde84928bd
sherlock <sherlock@lclark.edu>
2019-04-04 14:15:22 -0700

    Eli <3 Sherlock

--- src/Batch.java
+++ src/Batch.java
@@ -5,25 +5,24 @@ import java.util.Properties;
 
 public class Batch {
     //What does this do?
-    void run (boolean[][] colourmask) throws IOException {
+    //read in info from properties, store it variables
+    //path will be folder (for this)
+    //create loop through each image and give each variable and do not ask for user
+    //method 0 is Manuel, 1 is Nobis
+    void run (boolean[][] colourmask, String csvPath) throws IOException {
         Properties config = new Properties();
-        InputStream input = null;
-        //read in info from properties, store it variables
-        //path will be folder (for this)
-        //create loop through each image and give each variable and do not ask for user
-        //method 0 is Manuel, 1 is Nobis
-
-        input = new FileInputStream("config.properties");
+        InputStream input = new FileInputStream("config.properties");
         config.load(input);
+
         int threshold = Integer.parseInt(config.getProperty("threshold"));
         int radius = Integer.parseInt(config.getProperty("radius"));
         int xCenter = Integer.parseInt(config.getProperty("xCenter"));
         String path = config.getProperty("path");
-        //This needs to be done to turn the string into an array to be able to run through all of the paths
-        String[] paths = path.split(",");
+        String[] paths = path.split(","); //This needs to be done to turn the string into an array to be able to run through all of the paths
         int north = Integer.parseInt(config.getProperty("north"));
         int yCenter = Integer.parseInt(config.getProperty("yCenter"));
         int method = Integer.parseInt(config.getProperty("method"));
+
         Prop.deleteProperties();
         input.close();
 
@@ -32,8 +31,9 @@ public class Batch {
         if(temp.isDirectory()) {
             paths = temp.list((dir, name) -> name.toLowerCase().endsWith(".jpg"));
         }
-        double gapFraction;
+        double gapFraction = -1.0;
         for(int i=0; i<paths.length; i++){
+            String[] methods= {"Manual","Nobis"};
             BufferedImage original = ImageIO.read(new File(paths[i]));
             BufferedImage square = SquareTheCircle.buildASquare(original);
             if(method == 0) {
@@ -41,8 +41,15 @@ public class Batch {
                 gapFraction = Black.getGapFraction(black, colourmask);
             }
             else if(method == 1) {
-
+                BufferedImage black = Algorithms.nobis(square, colourmask);
+                gapFraction = Black.getGapFraction(black, colourmask);
             }
+            String[] data = new String[]{paths[i],methods[0],"N/A","",String.valueOf(gapFraction)};
+            if(method==0){
+                data[2]=String.valueOf(threshold);
+            }
+            //Write to the CSV
+            CSV.writeTo(csvPath,data);
         }
     }
 }
