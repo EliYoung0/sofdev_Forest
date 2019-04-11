c304a77d86c61929c4e17a915945050f90d91fce
eliyoung0 <eliyoung@lclark.edu>
2019-04-11 14:42:52 -0700

    Separated File path and name in csv

--- src/CSV.java
+++ src/CSV.java
@@ -4,11 +4,15 @@ abstract class CSV {
     static String write(String[] data) throws IOException {
         File csv = new File(data[0].substring(0,data[0].indexOf('.'))+ "_" + java.time.LocalDate.now()+".csv");
         FileWriter fw = new FileWriter(csv);
-        fw.write("Filename,Algorithm,Manual Threshold,ISF,Gap Fraction");
+        fw.write("Filepath,Filename,Algorithm,Manual Threshold,ISF,Gap Fraction");
         fw.append('\n');
-        for (String d:data) {
-            fw.append(d);
+
+        fw.append(data[0].substring(0,data[0].lastIndexOf('/')+1));
+        fw.append(',');
+        fw.append(data[0].substring(data[0].lastIndexOf('/')+1));
+        for (int i = 1; i < 5; i++) {
             fw.append(',');
+            fw.append(data[i]);
         }
         fw.close();
         return csv.getAbsolutePath();
@@ -17,9 +21,12 @@ abstract class CSV {
     static void writeTo(String path,String[] data) throws IOException {
         FileWriter fw = new FileWriter(path,true);
         fw.write('\n');
-        for (String d:data) {
-            fw.append(d);
+        fw.append(data[0].substring(0,data[0].lastIndexOf('/')+1));
+        fw.append(',');
+        fw.append(data[0].substring(data[0].lastIndexOf('/')+1));
+        for (int i = 1; i < 5; i++) {
             fw.append(',');
+            fw.append(data[i]);
         }
         fw.close();
     }
