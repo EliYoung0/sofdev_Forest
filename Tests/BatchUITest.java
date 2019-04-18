c56dbc33dba42cb83db1e70fa2df383e584415e8
eliyoung0 <eliyoung@lclark.edu>
2019-04-18 15:31:15 -0700

    Fixed Fileselector

---
+++ Tests/BatchUITest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BatchUITest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void sameIo(){
+
+    }
+}
