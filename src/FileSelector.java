02de0a3c7e31a4699f590ef927e14502dd3346ce
eliyoung0 <eliyoung@lclark.edu>
2019-04-23 14:58:45 -0700

    Fixed CSVTEst

--- src/FileSelector.java
+++ src/FileSelector.java
@@ -155,6 +155,7 @@ class FileSelector extends Container {
                     }
                     //Sets path for GUI to use to be the first file
                     path = files[0].getAbsolutePath();
+                    full=new String[]{f.getAbsolutePath()};
                     return true;
                 }
                 else {
