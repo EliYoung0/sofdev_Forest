f9d1020aa85f5d4910415e4ce36b083a0db9bba5
Eli Young <eliyoung@lclark.edu>
2019-04-30 08:45:13 -0700

    new test

---
+++ Test/FileSelectorTest.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileSelectorTest {
+    private UI ui;
+    @BeforeEach
+    void setUp() {
+        ui = new UI();
+        ui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    @Test
+    void singleFileTest(){
+        FileSelector f = (FileSelector)ui.getContentPane();
+        f.getAddress().setText("/Users/kepler/Desktop/a/1.jpg");
+        f.getOpen().doClick();
+        assertTrue(f.getGoodExit());
+        assertEquals(f.getFull(),new String[]{"/Users/kepler/Desktop/a/1.jpg"});
+    }
+
+    @Test
+    void multiFileTest(){
+        FileSelector f = (FileSelector)ui.getContentPane();
+        f.getAddress().setText("/Users/kepler/Desktop/a/1.jpg,/Users/kepler/Desktop/a/2.jpg");
+        f.getOpen().doClick();
+        assertTrue(f.getGoodExit());
+        assertEquals(f.getFull(),new String[]{"/Users/kepler/Desktop/a/1.jpg","/Users/kepler/Desktop/a/2.jpg"});
+    }
+
+    @Test
+    void directoryTest(){
+        FileSelector f = (FileSelector)ui.getContentPane();
+        f.getAddress().setText("/Users/kepler/Desktop/a");
+        f.getOpen().doClick();
+        assertEquals(f.getFull(),new String[]{"/Users/kepler/Desktop/a"});
+        assertTrue(f.getGoodExit());
+    }
+
+    @AfterEach
+    void tearDown() {
+        ui.dispose();
+    }
+}
